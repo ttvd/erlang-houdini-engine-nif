8a55a66cb81330ee4ffdb3cdbe07db1f08a51156
Mykola Konyk <mykola@konyk.org>
2015-04-13 11:06:53 -0400

    hapi_nif.c: Removing unused block.

--- c_src/hapi_nif.c
+++ c_src/hapi_nif.c
@@ -27,7 +27,7 @@ static ErlNifResourceType* hapi_handle;
 
 static
 ERL_NIF_TERM
-make_atom(ErlNifEnv* env, const char* atom_name)
+hapi_private_make_atom(ErlNifEnv* env, const char* atom_name)
 {
     ERL_NIF_TERM atom;
 
@@ -82,7 +82,7 @@ label_cleanup:
 int
 hapi_private_init(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
 {
-    g_atom_ok = make_atom(env, "ok");
+    g_atom_ok = hapi_private_make_atom(env, "ok");
 
     hapi_handle = enif_open_resource_type(env, "hapi", "hapi_handle", &hapi_private_cleanup,
         ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER, NULL);
@@ -108,82 +108,82 @@ hapi_private_process_result(ErlNifEnv* env, HAPI_Result result)
     {
         case HAPI_RESULT_SUCCESS:
         {
-            return make_atom(env, "hapi_result_success");
+            return hapi_private_make_atom(env, "hapi_result_success");
         }
 
         case HAPI_RESULT_FAILURE:
         {
-            return make_atom(env, "hapi_result_failure");
+            return hapi_private_make_atom(env, "hapi_result_failure");
         }
 
         case HAPI_RESULT_ALREADY_INITIALIZED:
         {
-            return make_atom(env, "hapi_result_already_initialized");
+            return hapi_private_make_atom(env, "hapi_result_already_initialized");
         }
 
         case HAPI_RESULT_NOT_INITIALIZED:
         {
-            return make_atom(env, "hapi_result_not_initialized");
+            return hapi_private_make_atom(env, "hapi_result_not_initialized");
         }
 
         case HAPI_RESULT_CANT_LOADFILE:
         {
-            return make_atom(env, "hapi_result_cant_loadfile");
+            return hapi_private_make_atom(env, "hapi_result_cant_loadfile");
         }
 
         case HAPI_RESULT_PARM_SET_FAILED:
         {
-            return make_atom(env, "hapi_result_parm_set_failed");
+            return hapi_private_make_atom(env, "hapi_result_parm_set_failed");
         }
 
         case HAPI_RESULT_INVALID_ARGUMENT:
         {
-            return make_atom(env, "hapi_result_invalid_argument");
+            return hapi_private_make_atom(env, "hapi_result_invalid_argument");
         }
 
         case HAPI_RESULT_CANT_LOAD_GEO:
         {
-            return make_atom(env, "hapi_result_cant_load_geo");
+            return hapi_private_make_atom(env, "hapi_result_cant_load_geo");
         }
 
         case HAPI_RESULT_CANT_GENERATE_PRESET:
         {
-            return make_atom(env, "hapi_result_cant_generate_preset");
+            return hapi_private_make_atom(env, "hapi_result_cant_generate_preset");
         }
 
         case HAPI_RESULT_CANT_LOAD_PRESET:
         {
-            return make_atom(env, "hapi_result_cant_load_preset");
+            return hapi_private_make_atom(env, "hapi_result_cant_load_preset");
         }
 
         case HAPI_RESULT_ASSET_DEF_ALREADY_LOADED:
         {
-            return make_atom(env, "hapi_result_asset_def_already_loaded");
+            return hapi_private_make_atom(env, "hapi_result_asset_def_already_loaded");
         }
 
         case HAPI_RESULT_NO_LICENSE_FOUND:
         {
-            return make_atom(env, "hapi_result_no_license_found");
+            return hapi_private_make_atom(env, "hapi_result_no_license_found");
         }
 
         case HAPI_RESULT_DISALLOWED_NC_LICENSE_FOUND:
         {
-            return make_atom(env, "hapi_result_disallowed_nc_license_found");
+            return hapi_private_make_atom(env, "hapi_result_disallowed_nc_license_found");
         }
 
         case HAPI_RESULT_DISALLOWED_NC_ASSET_WITH_C_LICENSE:
         {
-            return make_atom(env, "hapi_result_disallowed_nc_asset_with_c_license");
+            return hapi_private_make_atom(env, "hapi_result_disallowed_nc_asset_with_c_license");
         }
 
         case HAPI_RESULT_DISALLOWED_NC_ASSET_WITH_LC_LICENSE:
         {
-            return make_atom(env, "hapi_result_disallowed_nc_asset_with_lc_license");
+            return hapi_private_make_atom(env, "hapi_result_disallowed_nc_asset_with_lc_license");
         }
 
         case HAPI_RESULT_DISALLOWED_LC_ASSET_WITH_C_LICENSE:
         {
-            return make_atom(env, "hapi_result_disallowed_lc_asset_with_c_license");
+            return hapi_private_make_atom(env, "hapi_result_disallowed_lc_asset_with_c_license");
         }
 
         default:
@@ -211,8 +211,8 @@ hapi_initialize_impl_helper(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     int32_t nif_error = 0;
 
-    uint32_t otl_search_path_length = 0;
-    uint32_t dso_search_path_length = 0;
+    uint32_t otl_search_path_length = 0u;
+    uint32_t dso_search_path_length = 0u;
 
     char* otl_search_path = NULL;
     char* dso_search_path = NULL;
@@ -231,22 +231,22 @@ hapi_initialize_impl_helper(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
         goto label_cleanup;
     }
 
-    if(otl_search_path_length > 0)
+    if(otl_search_path_length > 0u)
     {
-        otl_search_path = malloc((otl_search_path_length + 1) * sizeof(char));
+        otl_search_path = malloc((otl_search_path_length + 1u) * sizeof(char));
 
-        if(enif_get_string(env, argv[0], otl_search_path, otl_search_path_length + 1, ERL_NIF_LATIN1) < 1)
+        if(enif_get_string(env, argv[0], otl_search_path, otl_search_path_length + 1u, ERL_NIF_LATIN1) < 1)
         {
             nif_error = 1;
             goto label_cleanup;
         }
     }
 
-    if(dso_search_path_length > 0)
+    if(dso_search_path_length > 0u)
     {
-        dso_search_path = malloc((dso_search_path_length + 1) * sizeof(char));
+        dso_search_path = malloc((dso_search_path_length + 1u) * sizeof(char));
 
-        if(enif_get_string(env, argv[1], dso_search_path, dso_search_path_length + 1, ERL_NIF_LATIN1) < 1)
+        if(enif_get_string(env, argv[1], dso_search_path, dso_search_path_length + 1u, ERL_NIF_LATIN1) < 1)
         {
             nif_error = 1;
             goto label_cleanup;
@@ -267,14 +267,6 @@ hapi_initialize_impl_helper(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
             goto label_cleanup;
         }
     }
-
-    /*
-
-    {
-        split_geos_by_group, max_vertices_per_primitive, refine_curve_to_linear, cureve_refine_lod,
-        clear_errors_and_warnings, cook_template_geos
-    }
-    */
 
     /*
     const char * otl_search_path,

ccf9204a18d02b6cec9284f38015d3f2596d6b4d
Mykola Konyk <mykola@konyk.org>
2015-04-13 11:31:45 -0400

    hapi_cook_options.hrl: Updating.

--- src/hapi_cook_options.hrl
+++ src/hapi_cook_options.hrl
@@ -3,5 +3,5 @@
 %%% @copyright 2015
 %%% HAPI_CookOptions structure.
 
--record(hapi_cook_options, {split_geos_by_group, max_vertices_per_primitive, refine_curve_to_linear, cureve_refine_lod,
+-record(hapi_cook_options, {split_geos_by_group, max_vertices_per_primitive, refine_curve_to_linear, curve_refine_lod,
     clear_errors_and_warnings, cook_template_geos}).

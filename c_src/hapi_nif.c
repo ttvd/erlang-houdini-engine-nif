c3ff2e3004690c8c62d5f2d2e4a1c3b1b9906fb9
Mykola Konyk <mykola@konyk.org>
2015-04-13 00:55:53 -0400

    Adding stubs for HAPI Initialize, Cleanup and IsInitialized.

--- c_src/hapi_nif.c
+++ c_src/hapi_nif.c
@@ -1,17 +1,38 @@
 #include "erl_nif.h"
-//#include "HAPI.h"
+#include "HAPI.h"
 
 int hapi_private_init(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info);
 void hapi_private_cleanup(ErlNifEnv* env, void* obj);
 
+ERL_NIF_TERM hapi_is_initialized_impl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+ERL_NIF_TERM hapi_initialize_impl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+ERL_NIF_TERM hapi_cleanup_impl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+
+
 static ErlNifFunc nif_funcs[] =
 {
-    //{"callname1", 2, func1},
-    //{"callname2", 1, func2}
+    {"hapi_is_initialized", 0, hapi_is_initialized_impl},
+    {"hapi_initialize", 5, hapi_initialize_impl},
+    {"hapi_cleanup", 0, hapi_cleanup_impl}
 };
+
 
 static ERL_NIF_TERM g_atom_ok;
 static ErlNifResourceType* hapi_handle;
+
+
+static ERL_NIF_TERM
+make_atom(ErlNifEnv* env, const char* atom_name)
+{
+    ERL_NIF_TERM atom;
+
+    if(enif_make_existing_atom(env, atom_name, &atom, ERL_NIF_LATIN1))
+    {
+        return atom;
+    }
+
+    return enif_make_atom(env, atom_name);
+}
 
 int
 hapi_private_init(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
@@ -29,3 +50,24 @@ hapi_private_init(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
 }
 
 ERL_NIF_INIT(hapi, nif_funcs, &hapi_private_init, NULL, NULL, NULL)
+
+
+ERL_NIF_TERM
+hapi_is_initialized_impl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    return make_atom(env, "ok");
+}
+
+
+ERL_NIF_TERM
+hapi_initialize_impl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    return make_atom(env, "ok");
+}
+
+
+ERL_NIF_TERM
+hapi_cleanup_impl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    return make_atom(env, "ok");
+}

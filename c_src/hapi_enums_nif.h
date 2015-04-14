4a44fa5bb369565801f232e379b5140886d67ecc
Mykola Konyk <mykola@konyk.org>
2015-04-14 12:46:27 -0400

    hapi_enums_nif.h: New header for enum conversion signatures.

---
+++ c_src/hapi_enums_nif.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#if !defined(HAPI_ENUMS_NIF_H)
+#define HAPI_ENUMS_NIF_H
+
+#include "erl_nif.h"
+#include "HAPI.h"
+
+// HAPI_State conversions.
+bool hapi_enum_state_erl_to_c(ErlNifEnv* env, const ERL_NIF_TERM term, HAPI_State* state);
+ERL_NIF_TERM hapi_enum_state_c_to_erl(ErlNifEnv* env, HAPI_State state);
+
+
+#endif //!defined(HAPI_ENUMS_NIF_H)

6b4017a14cebc5683abf1bb613ce6a91a7b541e1
Mykola Konyk <mykola@konyk.org>
2015-04-14 13:09:01 -0400

    hapi_enum_state_nif.c: Adding c to erl.

--- c_src/hapi_enum_state_nif.c
+++ c_src/hapi_enum_state_nif.c
@@ -12,5 +12,33 @@ hapi_enum_state_erl_to_c(ErlNifEnv* env, const ERL_NIF_TERM term, HAPI_State* st
 ERL_NIF_TERM
 hapi_enum_state_c_to_erl(ErlNifEnv* env, HAPI_State state)
 {
-    return hapi_private_make_atom(env, "ok");
+    switch(state)
+    {
+        case HAPI_STATUS_CALL_RESULT:
+        {
+            return hapi_private_make_atom(env, "hapi_status_call_result");
+        }
+
+        case HAPI_STATUS_COOK_RESULT:
+        {
+            return hapi_private_make_atom(env, "hapi_status_cook_result");
+        }
+
+        case HAPI_STATUS_COOK_STATE:
+        {
+            return hapi_private_make_atom(env, "hapi_status_cook_state");
+        }
+
+        case HAPI_STATUS_MAX:
+        {
+            return hapi_private_make_atom(env, "hapi_status_max");
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+
+    return enif_make_badarg(env);
 }

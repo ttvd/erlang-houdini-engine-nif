27ce06f36746032dd20db9f21f5598ed5a22b73d
Mykola Konyk <mykola@konyk.org>
2015-04-14 12:49:25 -0400

    hapi_enums_nif.h: Adding HAPI_Result signatures.

--- c_src/hapi_enums_nif.h
+++ c_src/hapi_enums_nif.h
@@ -6,6 +6,12 @@
 #include "erl_nif.h"
 #include "HAPI.h"
 
+
+// HAPI_Result conversions.
+bool hapi_enum_result_erl_to_c(ErlNifEnv* env, const ERL_NIF_TERM term, HAPI_Result* result);
+ERL_NIF_TERM hapi_enum_result_c_to_erl(ErlNifEnv* env, HAPI_Result result);
+
+
 // HAPI_State conversions.
 bool hapi_enum_state_erl_to_c(ErlNifEnv* env, const ERL_NIF_TERM term, HAPI_State* state);
 ERL_NIF_TERM hapi_enum_state_c_to_erl(ErlNifEnv* env, HAPI_State state);

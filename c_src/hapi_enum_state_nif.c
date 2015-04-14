40361ef7f59cb5191124c7c2b6d0a2f9c6ce0e81
Mykola Konyk <mykola@konyk.org>
2015-04-14 12:46:40 -0400

    hapi_enum_state.c: Adding stubs.

--- c_src/hapi_enum_state_nif.c
+++ c_src/hapi_enum_state_nif.c
@@ -1,7 +1,5 @@
-#include "erl_nif.h"
-#include "HAPI.h"
-
-#include <stdbool.h>
+#include "hapi_enums_nif.h"
+#include "hapi_private_nif.h"
 
 
 bool
@@ -14,5 +12,5 @@ hapi_enum_state_erl_to_c(ErlNifEnv* env, const ERL_NIF_TERM term, HAPI_State* st
 ERL_NIF_TERM
 hapi_enum_state_c_to_erl(ErlNifEnv* env, HAPI_State state)
 {
-
+    return hapi_private_make_atom(env, "ok");
 }

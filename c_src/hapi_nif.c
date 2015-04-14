6c8caf91d9629748abc16bec67dd12ca2fc2ab44
Mykola Konyk <mykola@konyk.org>
2015-04-13 23:25:19 -0400

    hapi_nif.c: Removing unused functions.

--- c_src/hapi_nif.c
+++ c_src/hapi_nif.c
@@ -5,9 +5,6 @@
 #include <string.h>
 
 
-int hapi_private_init(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info);
-void hapi_private_cleanup(ErlNifEnv* env, void* obj);
-
 ERL_NIF_TERM hapi_private_make_atom(ErlNifEnv* env, const char* atom_name);
 ERL_NIF_TERM hapi_private_get_boolean(ErlNifEnv* env, const ERL_NIF_TERM term, int32_t* value);
 
@@ -25,7 +22,6 @@ static ErlNifFunc nif_funcs[] =
 
 
 static ERL_NIF_TERM g_atom_ok;
-static ErlNifResourceType* hapi_handle;
 
 
 ERL_NIF_TERM
@@ -80,24 +76,7 @@ label_cleanup:
 }
 
 
-int
-hapi_private_init(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
-{
-    g_atom_ok = hapi_private_make_atom(env, "ok");
-
-    hapi_handle = enif_open_resource_type(env, "hapi", "hapi_handle", &hapi_private_cleanup,
-        ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER, NULL);
-
-    if(!hapi_handle)
-    {
-        return -1;
-    }
-
-    return 0;
-}
-
-
-ERL_NIF_INIT(hapi, nif_funcs, &hapi_private_init, NULL, NULL, NULL)
+ERL_NIF_INIT(hapi, nif_funcs, NULL, NULL, NULL, NULL)
 
 
 // Helper function to translate HAPI_Result into Erlang atoms.

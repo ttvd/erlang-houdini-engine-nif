dd7fdcff55699ce56cec68fe45bd82f3386224c5
Mykola Konyk <mykola@konyk.org>
2015-04-19 17:35:34 -0400

    hapi_nif.c: Adding SetParmFloatValue.

--- c_src/hapi_nif.c
+++ c_src/hapi_nif.c
@@ -1471,8 +1471,31 @@ hapi_set_parm_int_values_impl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[
 ERL_NIF_TERM
 hapi_set_parm_float_value_impl(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    // Needs implementation.
-    return hapi_enum_result_c_to_erl(env, HAPI_RESULT_SUCCESS);
+    HAPI_NodeId node_id = -1;
+    int32_t parm_index = 0;
+    double parm_value = 0;
+
+    if(hapi_private_get_hapi_node_id(env, argv[0], &node_id) &&
+        enif_get_int(env, argv[2], &parm_index) &&
+        enif_get_double(env, argv[3], &parm_value))
+    {
+        char* buffer = NULL;
+        uint32_t buffer_length = 0;
+
+        if(hapi_private_get_string(env, argv[1], &buffer, &buffer_length))
+        {
+            HAPI_Result result = HAPI_SetParmFloatValue(node_id, buffer, parm_index, (float) parm_value);
+
+            if(buffer)
+            {
+                free(buffer);
+            }
+
+            return hapi_enum_result_c_to_erl(env, result);
+        }
+    }
+
+    return enif_make_badarg(env);
 }
 
 // HAPI_SetParmFloatValues equivalent.

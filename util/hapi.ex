808644cbb1e527d175da2007600cdc63994ffa3e
Mykola Konyk <mykola@konyk.org>
2015-04-24 03:13:14 -0400

    util: hapi.ex: Adding extraction of enums from HAPI side.

--- util/hapi.ex
+++ util/hapi.ex
@@ -128,7 +128,7 @@ defmodule HAPI do
     # Print tokens.
     def print_tokens(tokens), do: Enum.map(tokens, fn(token) -> IO.inspect(token) end)
 
-    # Given a type map for mapping types from hapi.
+    # Given a list of tokens, produce a mapping table from hapi types.
     def type_map_hapi([]), do: []
     def type_map_hapi(tokens) do
         HashDict.new
@@ -158,6 +158,60 @@ defmodule HAPI do
     # Print from hapi type dictionary.
     def print_type_map_hapi(dict), do: Enum.map(dict, fn {k, v} -> IO.puts("#{k} -> #{v}") end)
 
+    # Given a list of tokens, produce a mapping table of hapi enums.
+    def enum_map_hapi([]), do: []
+    def enum_map_hapi(tokens), do: enum_map_hapi_collect(HashDict.new, tokens)
+
+    # Process tokens and collect enums.
+    defp enum_map_hapi_collect(dict, []), do: dict
+    defp enum_map_hapi_collect(dict, [:token_enum, enum_name, :token_bracket_curly_left | rest]) do
+        {enum_dict, remaining} = enum_map_hapi_extract(HashDict.new, rest, 0)
+        Dict.put(dict, enum_name, enum_dict) |> enum_map_hapi_collect(remaining)
+    end
+    defp enum_map_hapi_collect(dict, [_token | rest]), do: enum_map_hapi_collect(dict, rest)
+
+    # Helper function to extract enum values from token stream.
+    defp enum_map_hapi_extract(dict, [], _idx), do: {dict, []}
+    defp enum_map_hapi_extract(dict, [:token_comma | rest], idx), do: enum_map_hapi_extract(dict, rest, idx)
+    defp enum_map_hapi_extract(dict, [:token_bracket_curly_right, :token_semicolon | rest], _idx), do: {dict, rest}
+    defp enum_map_hapi_extract(dict, [enum_entry, :token_comma | rest], idx) do
+        enum_map_hapi_extract(Dict.put(dict, enum_entry, idx), rest, idx + 1)
+    end
+    defp enum_map_hapi_extract(dict, [enum_entry, :token_bracket_curly_right, :token_semicolon | rest], idx) do
+        {Dict.put(dict, enum_entry, idx), rest}
+    end
+    defp enum_map_hapi_extract(dict, [enum_entry, :token_assignment, enum_value | rest], _idx) when is_integer(enum_value) do
+        enum_map_hapi_extract(Dict.put(dict, enum_entry, enum_value), rest, enum_value + 1)
+    end
+    defp enum_map_hapi_extract(dict, [enum_entry, :token_assignment, enum_value | rest], _idx) do
+        #&(enum_map_hapi_extract(Dict.put(dict, enum_entry, &1), rest, &1 + 1)).(enum_map_hapi_lookup_value(dict, enum_value))
+        value = enum_map_hapi_lookup_value(dict, enum_value)
+        enum_map_hapi_extract(Dict.put(dict, enum_entry, value), rest, value + 1)
+    end
+
+    # Helper function used to look up enum value within enum table.
+    defp enum_map_hapi_lookup_value(dict, enum_value) do
+        if Dict.has_key?(dict, enum_value) do
+            orig_value = Dict.get(dict, enum_value)
+            if is_integer(orig_value) do
+                orig_value
+            else
+                enum_map_hapi_lookup_value(dict, orig_value)
+            end
+        else
+            raise(SyntaxError, description: "Unknown enum value assignment #{enum_value}")
+        end
+    end
+
+    # Print from hapi enums dictionary.
+    def print_enum_map_hapi(dict), do: Enum.map(dict, fn {k, v} -> print_enum_map_hapi(k, v) end)
+
+    # Helper function to print each enum.
+    defp print_enum_map_hapi(enum_name, enum_dict) do
+        IO.puts("#{enum_name}")
+        Enum.map(enum_dict, fn {k, v} -> IO.puts("    #{k} -> #{v}") end)
+        IO.puts("")
+    end
 end
 
 {:ok, data} = File.read("hapi.c.generated.osx")
@@ -165,5 +219,8 @@ data = HAPI.preprocess(data)
 data = HAPI.parse(data)
 #HAPI.print_tokens(data)
 
-types_from_hapi = HAPI.type_map_hapi(data)
-HAPI.print_type_map_hapi(types_from_hapi)
+#types_from_hapi = HAPI.type_map_hapi(data)
+#HAPI.print_type_map_hapi(types_from_hapi)
+
+types_enums_from_hapi = HAPI.enum_map_hapi(data)
+HAPI.print_enum_map_hapi(types_enums_from_hapi)

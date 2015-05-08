f12fab657437d1b753d5f6cd94feae881deb8fe1
Mykola Konyk <mykola@konyk.org>
2015-05-08 19:56:02 -0400

    test: test_cooking.erl: Updating.

--- test/test_cooking.erl
+++ test/test_cooking.erl
@@ -1,8 +1,8 @@
--module(test_instantiate).
+-module(test_cooking).
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("../src/hapi_records.hrl").
 
-instantiate_test() ->
+instantiate_and_cook_test() ->
     % Initialize HAPI.
     ?assertEqual(hapi_result_success, hapi:initialize("", "", hapi:cook_options_create(), true, -1)),
     % Load test_dummyboxes HDA.
@@ -16,10 +16,18 @@ instantiate_test() ->
     {HapiStatus, [AssetNameId]} = hapi:get_available_assets(LibraryId, AssetCount),
     % Get length of retrieved asset name.
     {HapiStatus, AssetNameLength} = hapi:get_string_buf_length(AssetNameId),
+    ?assertEqual(hapi_result_success, HapiStatus),
+    ?assertEqual(29, AssetNameLength),
     % Retrieve name of the asset.
-    {HapiStatus, _AssetName} = hapi:get_string(AssetNameId, AssetNameLength),
+    {HapiStatus, AssetNameBinary} = hapi:get_string(AssetNameId, AssetNameLength),
+    ?assertEqual(hapi_result_success, HapiStatus),
+    % We need to convert binary to string.
+    AssetName = binary_to_list(AssetNameBinary),
     % Instantiate the asset.
-    %{HapiStatus, AssetId} = hapi:instantiate_asset(AssetName, false),
+    {HapiStatus, AssetId} = hapi:instantiate_asset(AssetName, false),
+    ?assertEqual(hapi_result_success, HapiStatus),
+    % Cook the asset.
+    ?assertEqual(hapi_result_success, hapi:cook_asset(AssetId, hapi:cook_options_create())),
     % Clean up HAPI.
     ?assertEqual(hapi_result_success, hapi:cleanup()),
     ok.

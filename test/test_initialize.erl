b5e1e1c9e1e5ddc12bd827e8b33b1b1073477b4b
Mykola Konyk <mykola@konyk.org>
2015-05-08 02:28:37 -0400

    test: test_initialize.erl: Updating test.

--- test/test_initialize.erl
+++ test/test_initialize.erl
@@ -1,22 +1,29 @@
 -module(test_initialize).
 -include_lib("eunit/include/eunit.hrl").
--include_lib("../src/records/hapi_cookoptions.hrl").
+-include_lib("../src/hapi_records.hrl").
 
-nil_test() ->
-    %?assertEqual(hapi_result_success, hapi:initialize(nil, nil, nil, false, -1)),
-    %?assertEqual(hapi_result_success, hapi:is_initialized()),
-    %?assertEqual(hapi_result_success, hapi:cleanup()),
+% Attempt to initialize hapi, check initialization status and then clean up.
+initialize_true_test() ->
+    ?assertEqual(hapi_result_success, hapi:initialize("", "", hapi:cook_options_create(), true, -1)),
+    ?assertEqual(hapi_result_success, hapi:is_initialized()),
+    ?assertEqual(hapi_result_success, hapi:cleanup()),
     ok.
 
-record_test() ->
-    %CookOptions = #hapi_cookoptions{
-    %    split_geos_by_group=true,
-    %    max_vertices_per_primitive=3,
-    %    refine_curve_to_linear=false,
-    %    curve_refine_lod=1.0,
-    %    clear_errors_and_warnings=false,
-    %    cook_template_geos=false},
-    %?assertEqual(hapi_result_success, hapi:initialize(nil, nil, CookOptions, false, -1)),
-    %?assertEqual(hapi_result_success, hapi:is_initialized()),
-    %?assertEqual(hapi_result_success, hapi:cleanup()),
+% Create custom cook options record and use it for initialization.
+initialize_record_cookoptions_test() ->
+    CookOptions = #hapi_cook_options{
+        split_geos_by_group=true,
+        max_vertices_per_primitive=3,
+        refine_curve_to_linear=false,
+        curve_refine_lod=1.0,
+        clear_errors_and_warnings=false,
+        cook_templated_geos=false},
+    ?assertEqual(hapi_result_success, hapi:initialize("", "", CookOptions, true, -1)),
+    ?assertEqual(hapi_result_success, hapi:is_initialized()),
+    ?assertEqual(hapi_result_success, hapi:cleanup()),
+    ok.
+
+% Attempt to check if hapi is initialized when hapi has not been initialized.
+initialize_check_false_test() ->
+    ?assertEqual(hapi_result_not_initialized, hapi:is_initialized()),
     ok.
